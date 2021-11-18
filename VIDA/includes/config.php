2f7a6b279a06aaae7fac44ce1a2c280afa174aca
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 00:14:07 +0800

    Update config.php

--- VIDA/includes/config.php
+++ VIDA/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 //Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER","vidafeu.herokuapp.com");//define our database server
+defined('DB_SERVER') ? null : define("DB_SERVER","localhost");//define our database server
 defined('DB_USER') ? null : define("DB_USER","root");		  //define our database user	
 defined('DB_PASS') ? null : define("DB_PASS","");			  //define our database Password	
 defined('DB_NAME') ? null : define("DB_NAME","VIDAFEU"); //define our database Name
