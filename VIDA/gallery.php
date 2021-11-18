3dbafd4c2e39d26e77d152cf33265f11e3de7218
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-18 23:46:51 +0800

    home.php

---
+++ VIDA/gallery.php
@@ -0,0 +1,305 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<style type="text/css">
+	#background {
+		width: 100%;
+		height: 100%;
+		padding:1px;	
+	}
+	#background .stretch {
+		width: 100%;
+		height: 100%;
+		border: 1px solid #ddd; 
+
+	}
+</style>
+
+ <style>
+.buttonbooknow{
+  position: absolute;
+  top: 90%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #555;
+  color: white;
+  font-size: 30px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+}
+.buttonbooknow:hover {
+  background-color: Gold;
+}
+
+.discover_slider_container
+{
+  position: relative;
+  width: 150rem;
+  height: 40rem;
+  margin-top: 110px;
+  left: -58%;
+}
+
+.discover_slider_container2
+{
+  position: relative;
+  width: 150rem;
+  height: 20rem;
+  margin-top: 110px;
+  left: -58%;
+}
+
+.glass-container {
+  width: 100%;
+  height: auto;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2); 
+  border-radius: 5px;
+  position: relative;
+  z-index: 1;
+  background: inherit;
+  overflow: hidden;
+  top: -250px;
+}
+
+.glass-container:before {
+  content: "";
+  position: absolute;
+  background: inherit;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: inset 0 0 2000px rgba(255, 255, 255, .5);
+  filter: blur(10px);
+  margin: -20px;
+}
+
+.centrepositioning
+{
+    border:thin blue solid;
+    margin:auto;
+    padding:10px;
+    }
+.howToLeftList
+    {
+    width:250px;
+    height:300px;
+    background-color:#004FB6;
+    padding:10px;
+    color:white;
+    float:left;
+    margin:5px;
+}
+
+
+</style>
+<div id="slide1"  style="display:none"> <!------------------------------------------------------------------ STANDARD ROOM ---------------------------------------------------------------------->
+<div class="discover_slider_container2">
+<div class="glass-container">
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">           
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/discover_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/discover_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/discover_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+             <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/discover_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  	</div>
+  </div>
+
+
+<div id="slide3"  style="display:none"> <!------------------------------------------------------------------ SUPER DELUXE ROOM ---------------------------------------------------------------------->
+<div class="discover_slider_container2">
+<div class="glass-container">
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">           
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/room_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/room_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/room_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+             <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/room_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  	</div>
+  </div>
+
+<div id="slide2"  style="display:none">          <!------------------------------------------------------------------ DELUXE ROOM ---------------------------------------------------------------------->
+<div class="discover_slider_container2">
+<div class="glass-container">
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">           
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/offer_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/offer_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/offer_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+             <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/offer_4.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  	</div>
+  </div>
+
+
+ </div>
+<div id="main" class="site-main clr"> 
+    <div id="primary" class="content-area clr"> 
+        <div id="content-wrap">
+        	<div class="row">
+				<div class="col-lg-4">
+					<div id="background">
+						<img class="stretch" src="<?php echo WEB_ROOT; ?>images/SlideShow/offer_1.jpg">
+						 <button id="showpanel1" class="buttonbooknow">STANDARD ROOM</a></button> 
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div id="background">
+						<img class="stretch img-hover" src="<?php echo WEB_ROOT; ?>images/SlideShow/offer_2.jpg">
+						<button id="showpanel2" class="buttonbooknow">DELUXE ROOM</a></button> 
+					</div>
+				</div> 
+				<div class="col-lg-4">
+					<div id="background">
+						<img class="stretch img-hover" src="<?php echo WEB_ROOT; ?>images/SlideShow/offer_3.jpg">
+						<button id="showpanel3" class="buttonbooknow">SUPER DELUXE ROOM</a></button> 
+					</div>
+				</div>
+        	</div>
+    	</div>
+	</div>
+</div>
+<br></br>
+<br></br>
+<br></br>
+<script>
+$(function() {
+    $('#showpanel1').click(function() {
+        $('div[id^=slide2]').hide();
+        $('div[id^=slide3]').hide();
+        $('#slide1').show();
+    });
+    $('#showpanel2').click(function() {
+        $('div[id^=slide1]').hide();
+        $('div[id^=slide3]').hide();
+        $('#slide2').show();
+    });
+	$('#showpanel3').click(function() {
+		 $('div[id^=slide2]').hide();
+        $('div[id^=slide1]').hide();
+        $('#slide3').show();
+    });
+
+
+})
+</script>
