345024d625929bcca4076e237c29c39930d49577
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 01:29:38 +0800

    UPDATE

--- VIDA/includes/functions.php
+++ VIDA/includes/functions.php
@@ -36,7 +36,7 @@
 	}
 	function __autoload($class_name) {
 		$class_name = strtolower($class_name);
-		$path = LIB_PATH.DS."{$class_name}.php";
+		$path = "/app/includes/{$class_name}.php";
 		if(file_exists($path)){
 			require_once($path);
 		}else{
