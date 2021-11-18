086793643b34e549a775148a577bfb0fff273e80
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 00:22:00 +0800

    update

--- VIDA/index.php
+++ VIDA/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once("includes/initialize.php");
-require_once("includes/config.php");
 $content='home.php';
 $view = (isset($_GET['p']) && $_GET['p'] != '') ? $_GET['p'] : '';
 $account = 'guest/update.php';
