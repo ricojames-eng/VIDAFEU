664b8b10d8fbb96dd27fba8d4c07e6f81af28a50
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 00:35:23 +0800

    update

--- VIDA/includes/config.php
+++ VIDA/includes/config.php
@@ -9,7 +9,7 @@ $thisFile = str_replace('\\', '/', __FILE__);
 $docRoot =$_SERVER['DOCUMENT_ROOT'];
 
 $webRoot  = str_replace(array($docRoot, 'includes/config.php'), '', $thisFile);
-$srvRoot  = str_replace('includes/config.php','', $thisFile);
+$srvRoot  = str_replace('config/config.php','', $thisFile);
 
 define('WEB_ROOT', $webRoot);
 define('SRV_ROOT', $srvRoot);
