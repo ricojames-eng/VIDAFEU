f0b1ffebe901e3f319edd3b11fcf3d8a3c0bcabe
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 01:15:31 +0800

    UP

--- VIDA/includes/initialize.php
+++ VIDA/includes/initialize.php
@@ -4,18 +4,18 @@ defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.
 defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'includes');
 
 // load config file first 
-//require_once("/app/VIDA/includes/config.php");
+require_once(LIB_PATH.DS."config.php");
 //load basic functions next so that everything after can use them
-require_once("/app/VIDA/includes/functions.php");
+require_once(LIB_PATH.DS."functions.php");
 //later here where we are going to put our class session
-require_once("/app/VIDA/includes/session.php");
-require_once("/app/VIDA/includes/user.php"); 
-require_once("/app/VIDA/includes/accomodation.php");
-require_once("/app/VIDA/includes/guest.php"); 
-require_once("/app/VIDA/includes/reserve.php");  
+require_once(LIB_PATH.DS."session.php");
+require_once(LIB_PATH.DS."user.php"); 
+require_once(LIB_PATH.DS."accomodation.php");
+require_once(LIB_PATH.DS."guest.php"); 
+require_once(LIB_PATH.DS."reserve.php");  
 //Load Core objects
 
-require_once("database.php");
+require_once(LIB_PATH.DS."database.php");
 
 //load database-related classes
 ?>
