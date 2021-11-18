ad7736ab9566510ae45a075fac69ae4597c87bc8
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-19 01:08:30 +0800

    update

--- VIDA/includes/config.php
+++ VIDA/includes/config.php
@@ -5,7 +5,7 @@ defined('DB_USER') ? null : define("DB_USER","root");		  //define our database u
 defined('DB_PASS') ? null : define("DB_PASS","");			  //define our database Password	
 defined('DB_NAME') ? null : define("DB_NAME","VIDAFEU"); //define our database Name
 
-$thisFile = str_replace(__FILE__);
+$thisFile = str_replace('\\', '/', __FILE__);
 $docRoot =$_SERVER['DOCUMENT_ROOT'];
 
 $webRoot  = str_replace(array($docRoot, 'includes/config.php'), '', $thisFile);
