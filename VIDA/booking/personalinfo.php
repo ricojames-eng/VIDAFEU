ba211da3deb7c4edb01f571558aba80860a06758
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-20 03:14:07 +0800

    Update personalinfo.php

--- VIDA/booking/personalinfo.php
+++ VIDA/booking/personalinfo.php
@@ -3,24 +3,24 @@ require_once("/app/includes/initialize.php");
 
 if (isset($_POST['submit'])){
 
-	  @$A = 0;
-         @$B = 'FREE'; 
-         @$C = 'FREE'; 
-         @$D = 'FREE'; 
-         @$E = 'FREE'; 
-         @$F = '03/15/1998';
-         @$G = '099999999';
-         @$H = 'FREE'; 
-         @$I = 'FREE'; 
-         @$J = 'FREE'; 
+	  	 @$A = 0;
+         @$B = $_POST['name']; 
+         @$C = $_POST['last'];
+         @$D = $_POST['city'];
+         @$E = $_POST['address'];
+         @$F = $_POST['dbirth'];
+         @$G = $_POST['phone'];
+         @$H = $_POST['nationality'];
+         @$I = $_POST['company'];
+         @$J = $_POST['caddress'];
          @$K = 1;
-         @$L = 'FREE@gmail.com'; 
-         @$M = 'FREE'; 
-         @$N = 'FREE'; 
-         @$O = 'FREE'; 
+         @$L = $_POST['cemail'];
+         @$M = $_POST['username'];
+         @$N = $_POST['pass'];
+         @$O = $_POST['zip'];
 
       global $mydb;
-      $sql = "INSERT INTO `tblguest`(`REFNO`, `G_FNAME`, `G_LNAME`, `G_CITY`, `G_ADDRESS`, `DBIRTH`, `G_PHONE`, `G_NATIONALITY`, `G_COMPANY`, `G_CADDRESS`, `G_TERMS`, `G_EMAIL`, `G_UNAME`, `G_PASS`, `ZIP`) VALUES ('$A','$B','$C','$D','$E','$F','$G','$H','$I','$J','$K','$L','$M','$N','$O')";
+      $sql = "INSERT INTO `tblguest`(`REFNO`, `G_FNAME`, `G_LNAME`, `G_CITY`, `G_ADDRESS`, `DBIRTH`, `G_PHONE`, `G_NATIONALITY`, `G_COMPANY`, `G_CADDRESS`, `G_TERMS`, `G_EMAIL`, `G_UNAME`, `G_PASS`, `ZIP`) VALUES (0,'$B','$C','$D','$E','$F','$G','$H','$I','$J',1,'$L','$M','$N','$O')";
       $mydb->setQuery($sql);
       $mydb->executeQuery(); 
 
