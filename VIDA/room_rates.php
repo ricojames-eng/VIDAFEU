22f0ac6a992a7c4bdd17180d4e87d140cc545555
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-22 04:58:52 +0800

    Update room_rates.php

--- VIDA/room_rates.php
+++ VIDA/room_rates.php
@@ -175,7 +175,7 @@
                     </select>
                   </div>
                 </div>
-                <center><a></a></center>
+                <center><a> </a></center>
                 <button class="booking_form_button ml-lg-auto">FIND A ROOM</button>
               </div>
             </form>
@@ -385,7 +385,7 @@ $_SESSION['departure'] =date_format(date_create($_POST['departure']),"Y-m-d");
                   <input type="hidden" name="ROOMID" value="<?php echo $result->ROOMID ;?>">
 
                       <div class="card">
-                        <img class="card-img-top"  src="<?php echo WEB_ROOT .'mod_room/'.$result->ROOMIMAGE; ?>" alt="Room image description">
+                        <img class="card-img-top"  src="<?php echo WEB_ROOT .'admin/mod_room/'.$result->ROOMIMAGE; ?>" alt="Room image description">
                         <div class="card-body">
                           <div class="rooms_title"><h2><?php echo $result->ROOM ;?> <?php echo $result->ACCOMODATION ;?></h2></div>
                           <div class="rooms_text">
