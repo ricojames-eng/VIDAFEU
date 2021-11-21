d1bd66e912021adc3fbbea2ee9b945c1a58f3585
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-22 04:40:45 +0800

    Update room_rates.php

--- VIDA/room_rates.php
+++ VIDA/room_rates.php
@@ -147,9 +147,10 @@
             <form action="<?php echo WEB_ROOT;?>index.php?p=booking" method="POST" class="booking_form" autocomplete="off">
               <div class="booking_form_container d-flex flex-lg-row flex-column align-items-start justify-content-start flex-wrap">
                 <div class="booking_form_inputs d-flex flex-row align-items-start justify-content-between flex-wrap">
-                  <div class="booking_dropdown"><input type="text" class="datepicker booking_input booking_input_a booking_in" placeholder="Check in" name="arrival" required="required" value="<?php echo isset($_POST['arrival']) ? $_POST['arrival'] :date('m/d/Y');?>"></div>
-                  <div class="booking_dropdown"><input type="text" class="datepicker booking_input booking_input_a booking_out" placeholder="Check out" name="departure" required="required" value="<?php echo isset($_POST['departure']) ? $_POST['departure'] : date('m/d/Y');?>" ></div>
+                  <div class="booking_dropdown"><center><a>Arrival Date</a></center><input type="text" class="datepicker booking_input booking_input_a booking_in" placeholder="Check in" name="arrival" required="required" value="<?php echo isset($_POST['arrival']) ? $_POST['arrival'] :date('m/d/Y');?>"></div>
+                  <div class="booking_dropdown"><center><a>Departure Date</a></center><input type="text" class="datepicker booking_input booking_input_a booking_out" placeholder="Check out" name="departure" required="required" value="<?php echo isset($_POST['departure']) ? $_POST['departure'] : date('m/d/Y');?>" ></div>
                   <div class="custom-select">
+                    <center><a>Accomodation</a></center>
                           <?php
                          $accomodation = New Accomodation();
                          $cur = $accomodation->listOfaccomodation(); 
