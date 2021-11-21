163bae19ca5e950efc55e7213015113c124ddd1b
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-22 01:27:11 +0800

    Update room_rates.php

--- VIDA/room_rates.php
+++ VIDA/room_rates.php
@@ -61,69 +61,6 @@
   filter: blur(10px);
   margin: -20px;
 }
-
-.input,
-.textarea {
-  border: 1px solid #eeeeee;
-  box-sizing: border-box;
-  margin: 0;
-  outline: none;
-  padding: 10px;
-}
-
-.input[type="button"] {
-  -webkit-appearance: button;
-  cursor: pointer;
-}
-
-.input::-webkit-outer-spin-button,
-.input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-}
-
-.input-group {
-  clear: both;
-  margin: 15px 0;
-  position: relative;
-}
-
-.input-group input[type='button'] {
-  background-color: #eeeeee;
-  min-width: 38px;
-  width: auto;
-  transition: all 300ms ease;
-}
-
-.input-group .button-minus,
-.input-group .button-plus {
-  font-weight: bold;
-  height: 38px;
-  padding: 0;
-  width: 38px;
-  position: relative;
-}
-
-.input-group .quantity-field {
-  position: relative;
-  height: 38px;
-  left: -6px;
-  text-align: center;
-  width: 62px;
-  display: inline-block;
-  font-size: 13px;
-  margin: 0 0 5px;
-  resize: vertical;
-}
-
-.button-plus {
-  left: -13px;
-}
-
-.input[type="number"] {
-  -moz-appearance: textfield;
-  -webkit-appearance: none;
-}
-
 </style>
 
 <div class="discover_slider_container2">
@@ -218,18 +155,11 @@
                   
 
                   <div class="custom-select">
-                    <!--
-                    <select name="person" id="person">
-                      <option value="0">Adult - Child</option>                   
-                    </select> -->
-
-                    <div class="input-group">
-                      <input type="button" value="-" class="button-minus" data-field="quantity">
-                      <input type="number" step="1" max="" value="1" name="quantity" class="quantity-field">
-                      <input type="button" value="+" class="button-plus" data-field="quantity">
-                    </div>
+                    <select <input type="number" min="0" name="person" id="person">
+                      <option value="0">Persons</option>
+                    </select>
                   </div>
-                 
+                  
 
                   <div class="custom-select">
                           <?php
@@ -485,39 +415,3 @@ $_SESSION['departure'] =date_format(date_create($_POST['departure']),"Y-m-d");
           </div>
  </div>
 
-<script>
-function incrementValue(e) {
-  e.preventDefault();
-  var fieldName = $(e.target).data('field');
-  var parent = $(e.target).closest('div');
-  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-
-  if (!isNaN(currentVal)) {
-    parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
-  } else {
-    parent.find('input[name=' + fieldName + ']').val(0);
-  }
-}
-
-function decrementValue(e) {
-  e.preventDefault();
-  var fieldName = $(e.target).data('field');
-  var parent = $(e.target).closest('div');
-  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-
-  if (!isNaN(currentVal) && currentVal > 0) {
-    parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
-  } else {
-    parent.find('input[name=' + fieldName + ']').val(0);
-  }
-}
-
-$('.input-group').on('click', '.button-plus', function(e) {
-  incrementValue(e);
-});
-
-$('.input-group').on('click', '.button-minus', function(e) {
-  decrementValue(e);
-});
-
-</script>
