2720bd85f221558d19cf7936ee7dbca9c36954d3
ricojames-eng <ricojames.quirante@lorma.edu>
2021-11-23 02:28:58 +0800

    Update controller.php

--- VIDA/admin/mod_room/controller.php
+++ VIDA/admin/mod_room/controller.php
@@ -67,7 +67,6 @@ function doInsert(){
  				$room->Amenities	=	$_POST['AMMENITIES'];
 				$room->RateXtraAdult	=	$_POST['RATEXTRAADULT'];
 				$room->RateXtraKid	=	$_POST['RATEXTRAKID'];
-
 				$istrue = 1;
 
 						@$ROOMNUM =	$_POST['ROOMNUM'];
@@ -85,7 +84,7 @@ function doInsert(){
 						@$RateXtraKid =	$_POST['RATEXTRAKID'];
 
 						global $mydb;
-					 	$sql = "INSERT INTO tblroom (ROOMNUM, ACCOMID, ROOM, ROOMDESC, NUMPERSON, PRICE, ROOMIMAGE, OROOMNUM, RoomTypeID, RateType, SeasonName, Amenities, RateXtraAdult, RateXtraKid) VALUES ('$ROOMNUM','$ACCOMID','$ROOM','$ROOMDESC','$NUMPERSON','$PRICE','$ROOMIMAGE','$OROOMNUM','0','$RateType','$SeasonName','$Amenities','$RateXtraAdult','$RateXtraKid')";  // Booking_id = GUEST NUMBER;
+					 	$sql = "INSERT INTO tblroom (ROOMNUM, ACCOMID, ROOM, ROOMDESC, NUMPERSON, PRICE, ROOMIMAGE, OROOMNUM, RoomTypeID, RateType, SeasonName, Amenities, RateXtraAdult, RateXtraKid) VALUES ('$ROOMNUM','$ACCOMID','$ROOM','$ROOMDESC','$NUMPERSON','$PRICE','rooms/$image_name','$OROOMNUM','0','$RateType','$SeasonName','$Amenities','$RateXtraAdult','$RateXtraKid')";  // Booking_id = GUEST NUMBER;
       					$mydb->setQuery($sql);
       					$mydb->executeQuery(); 
 
