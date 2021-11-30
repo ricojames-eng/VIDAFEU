399c821916cfd684984a74bf5fdf24584e698055
ricojames-eng <ricojames.quirante@lorma.edu>
2021-12-01 05:13:20 +0800

    Update template.php

--- VIDA/theme/template.php
+++ VIDA/theme/template.php
@@ -12,7 +12,7 @@ $_SESSION['to']  = $_POST['to'];
 <title>VIDA HOTEL</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Marimar Hotel template project">
+<meta name="description" content="VIDA HOTEL FEU STUDENTS PROJECT">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT;?>styles/bootstrap-4.1.2/bootstrap.min.css">
 <link href="<?php echo WEB_ROOT;?>plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
